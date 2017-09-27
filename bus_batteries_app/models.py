62a40445e13bc73868c61d9d5fb8723054d4d91d
Paweł Kordowski <paw.mar.kor@gmail.com>
2017-09-27 15:55:57 +0200

    Add models

--- bus_batteries_app/models.py
+++ bus_batteries_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Bus(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+
+class Battery(models.Model):
+    db_id = models.BigIntegerField(primary_key=True)
+    id = models.BigIntegerField()
+    number = models.BigIntegerField()
+    active = models.BooleanField()
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
